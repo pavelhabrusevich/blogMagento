bea7fe9ad32f393dec328b6a76241ebdafb2e9b0
gabrusevich <pavel.gabrusevich@amasty.com>
2020-06-14 21:08:26 +0300

    added posts with style

--- Blog/view/frontend/web/js/blog.js
+++ Blog/view/frontend/web/js/blog.js
@@ -1,13 +1,23 @@
 define(['uiComponent'], function (Component) {
     'use strict';
+
     return Component.extend({
         defaults: {
             template: 'Habr_Blog/blog'
         },
+
         initialize: function () {
             this._super();
-            console.log(this.title);
+
             return this
+        },
+
+        getDate: function (value) {
+            const date = new Date(value);
+            const formatter = new Intl.DateTimeFormat('en', {month: "short"});
+            const month = formatter.format(date);
+
+            return month + ' ' + date.getDate() + ' ' + date.getFullYear();
         }
     });
 });
